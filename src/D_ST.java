1827218a998a500b3297552cdf9e0305838c0d54
masonv-0 <vandermeermason@gmail.com>
2024-10-07 16:28:14 -0500

    Finished rough draft of algorithm
    Took out the parent class and moved all data to each individual class. Just seemed like too much work to get the infrastructure needed to transfer information between parent and child classes. Finished the calculations for each position's points and labeled how each factor contributes. Last, changed red zone touches to average instead of total.

--- src/D_ST.java
+++ src/D_ST.java
@@ -1,19 +1,65 @@
-public class D_ST extends Player{
+public class D_ST{
 
+    private String teamName;
+    private int currentWeek;
+    private String currentOpponent;
     private double[] weeklyStats;
     private int opposingOffenseRank;
 
-    D_ST (String t, String c, double[] w, int o) {
-        super(t, c);
+    D_ST (String t, int cw, String c, double[] w, int o) {
+        teamName = t;
+        currentWeek = cw;
+        currentOpponent = c;
         weeklyStats = w;
         opposingOffenseRank = o;
     }
 
-    public void printAll() {
-        super.printAll();
+    public void calculatePoints() {
+        //Finds average points over the last 16 weeks
+        double totalPoints = 0;
         for (int i = 0;i<16;i++) {
-            System.out.print(weeklyStats[i] + " ");
+            totalPoints+=weeklyStats[i];
         }
-        System.out.println(opposingOffenseRank);
+
+        double expectedPoints = 5.0;
+        //Factors in opposing offense rank
+        expectedPoints-=getRanges(opposingOffenseRank);
+
+        System.out.println();
+        System.out.println("The " + teamName + " D/ST is projected " + expectedPoints + " in Week " + currentWeek + " against " + currentOpponent);
+    }
+
+    public double getRanges(int rank) {
+        if (rank < 5) {
+            return 3.0;
+        }
+
+        else if (rank > 4 && rank < 9) {
+            return 2.0;
+        }
+
+        else if (rank > 8 && rank < 13) {
+            return 1.0;
+        }
+
+        else if (rank > 12 && rank < 17) {
+            return 0;
+        }
+
+        else if (rank > 16 && rank <21) {
+            return -1.5;
+        }
+
+        else if (rank > 20 && rank < 25) {
+            return -2.5;
+        }
+
+        else if (rank > 24 && rank < 29) {
+            return -3.5;
+        }
+
+        else {
+            return -4.5;
+        }
     }
 }
